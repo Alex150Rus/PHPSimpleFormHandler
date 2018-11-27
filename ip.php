532a3f08e01a57f87e522b85c634141d4b78c7eb
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2018-11-27 11:40:52 +0300

    this php shows ip and browser

---
+++ ip.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Вывод ip и браузера пользователя</title>
+</head>
+<body>
+    Ваш IP адрес: <?= $_SERVER['REMOTE_ADDR']?><br>
+    Ваш браузер: <?= $_SERVER['HTTP_USER_AGENT']?>
+</body>
+</html>
